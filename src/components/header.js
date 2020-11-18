64460cb08b2f8c9cc44b81940d43171dfb26fc5c
Kostas Vrouvas <kosvrouvas@gmail.com>
2020-11-18 22:49:25 +0200

    Update to TailwindCSSv2 and PostCSS 8

--- src/components/header.js
+++ src/components/header.js
@@ -6,19 +6,19 @@ function Header({ siteTitle }) {
   const [isExpanded, toggleExpansion] = useState(false)
 
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-gradient-to-r from-purple-800 to-teal-500 mb-6 p-6">
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <span className="font-semibold text-xl tracking-tight">
+    <nav className="flex flex-wrap items-center justify-between p-6 mb-6 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+      <div className="flex items-center flex-shrink-0 mr-6 text-white">
+        <span className="text-xl font-semibold tracking-tight">
           {siteTitle}
         </span>
       </div>
       <div className="block lg:hidden">
         <button
           onClick={() => toggleExpansion(!isExpanded)}
-          className="flex items-center px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white"
+          className="flex items-center px-3 py-2 text-white border border-white rounded hover:text-white hover:border-white"
         >
           <svg
-            className="fill-current h-3 w-3"
+            className="w-3 h-3 fill-current"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
           >
@@ -36,13 +36,13 @@ function Header({ siteTitle }) {
           <Link
             to={`/`}
             href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4"
+            className="block mt-4 mr-4 text-white lg:inline-block lg:mt-0 hover:text-white"
           >
             Home
           </Link>
           <Link
             to={`/page-2`}
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4"
+            className="block mt-4 mr-4 text-white lg:inline-block lg:mt-0 hover:text-white"
           >
             page 2
           </Link>
@@ -50,7 +50,7 @@ function Header({ siteTitle }) {
         <div>
           <a
             href="#download"
-            className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-black hover:bg-white mt-4 lg:mt-0"
+            className="inline-block px-4 py-2 mt-4 text-sm leading-none text-white border border-white rounded hover:border-transparent hover:text-black hover:bg-white lg:mt-0"
           >
             Download
           </a>
