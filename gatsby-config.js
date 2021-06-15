82aa2e95814b5f7b26d281de3725fb41d6fa3853
Vagelis Papaioannou <info@eboxnet.com>
2021-06-15 13:52:08 +0300

    removed image component ( deprecated ) added gatsby-plugin-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,8 +16,9 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
+    `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
